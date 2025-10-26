efe2506d3ace65d78a0f4d785fe733b680d6a64e
Thong Tue Duong <tueduong005@gmail.com>
2025-10-26 16:56:16 +0700

    chore: init

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {...} @ inputs: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+              inputs.self.overlays.default
+            ];
+          };
+        });
+  in {
+    overlays.default = final: prev: {
+      rustToolchain = prev.rust-bin.stable.latest.default.override {
+        extensions = ["rust-src" "rustfmt"];
+      };
+    };
+
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          openssl
+          pkg-config
+
+          cargo-audit
+          cargo-deny
+          cargo-edit
+          cargo-watch
+          rust-analyzer
+          rustToolchain
+        ];
+
+        env = {
+          RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+        };
+      };
+    });
+  };
+}
